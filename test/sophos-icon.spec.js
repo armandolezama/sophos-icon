d7dd6af22eab96f5efe38fe71fb2be2acb2ed026
armando <armando.lezama.psic@gmail.com>
2021-03-05 10:49:19 -0600

    chore(*):initial commit

---
+++ test/sophos-icon.spec.js
@@ -0,0 +1,23 @@
+/* eslint-disable no-unused-expressions */
+import { fixture, assert } from "@open-wc/testing";
+import '../sophos-icon';
+
+describe('Suite cases', () => {
+    it('First Test', async () => {
+        const _element = await fixture('<sophos-card></sophos-card>');
+        const data = {
+        iconText : 'Icon text',
+        imageSource : './assets/contact-icon.png',
+        imageAlt : 'not picture',
+        iconDirection : 'left'
+        }
+        _element.iconText = data.iconText;
+        _element.imageSource = data.imageSource;
+        _element.imageAlt = data.imageSource;
+        _element.iconDirection = data.iconDirection;
+        assert.strictEqual(_element.iconText, data.iconText);
+        assert.strictEqual(_element.imageSource, data.imageSource);
+        assert.strictEqual(_element.imageAlt, data.imageSource);
+        assert.strictEqual(_element.iconDirection, data.iconDirection);
+    });
+});

9548a6346d0dee80314365e650d11b03568756ac
armandolezama <armando.lezama.psic@gmail.com>
2021-12-16 01:27:54 -0600

    Major(*): Updated lit and added eslint rulez

--- sophos-icon.js
+++ sophos-icon.js
@@ -1,4 +1,4 @@
-import { LitElement, html } from 'lit-element';
+import { LitElement, html } from 'lit';
 import styles from './sophos-icon-styles';
 
 export class SophosIcon extends LitElement {
@@ -8,35 +8,35 @@ export class SophosIcon extends LitElement {
     * state, set up event listeners, create shadow dom.
     * @constructor
     */
-  constructor() {
+  constructor () {
     super();
     this.iconText = '';
     this.imageSource = '';
     this.imageAlt = '';
     this.iconDirection = '';
-  };
+  }
 
   /**
     * Declared properties and their corresponding attributes
     */
-  static get properties() {
+  static get properties () {
     return {
       iconText : {type : String},
       imageSource : {type : String},
       imageAtl : {type : String},
       iconDirection : { type : String}
     };
-  };
+  }
 
-  static get styles() {
+  static get styles () {
     return styles;
-  };
+  }
 
-  render() {
+  render () {
     return html`
-      <div id="main-container" icon-direction="${this.iconDirection}">
+      <div id="main-container" icon-direction=${this.iconDirection}>
         <h3 id="icon-text">${this.iconText}</h3>
-        <img id="icon-image" src="${this.imageSource}" alt="${this.imageAlt}">
+        <img id="icon-image" src=${this.imageSource} alt=${this.imageAlt}>
       </div>
     `;
   }

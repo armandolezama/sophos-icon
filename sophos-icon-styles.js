6c49a873effcffb3a799d9d95034a8d2cbe98e08
armandolezama <armando.lezama.psic@gmail.com>
2021-12-24 14:54:26 -0600

    feat(js): added styles for containers and elements

--- sophos-icon-styles.js
+++ sophos-icon-styles.js
@@ -5,16 +5,32 @@ export default css `
     display: var(--sophos-icon-main-container-display, inline-flex);
     justify-content: var(--sophos-icon-main-container-justify-content, center);
     align-items: var(--sophos-icon-main-container-align-items, center);
+    margin: var(--sophos-icon-main-container-margin, auto);
+    padding: var(--sophos-icon-main-container-padding, auto);
+    border: var(--sophos-icon-main-container-border, none);
+    border-radius: var(--sophos-icon-main-container-border-radius, none);
+    cursor: var(--sophos-icon-main-container-cursor, auto);
   }
 
   #icon-text {
     text-align: var(--sophos-icon-icon-text-text-align, center);
     font-size: var(--sophos-icon-icon-text-font-size, 16px);
+    margin: var(--sophos-icon-icon-text-margin, auto);
+    border-radius: var(--sophos-icon-icon-text-border-radius, 0);
+    border: var(--sophos-icon-icon-text-border, none);
+    color: var(--sophos-icon-icon-text-color, #000000);
+    background: var(--sophos-icon-icon-text-background, transparent);
+    cursor: var(--sophos-icon-icon-text-cursor, auto);
   }
 
   #icon-image {
+    box-sizing: var(--sophos-icon-icon-image-box-sizing, content-box);
     width: var(--sophos-icon-icon-image-width, 50px);
     height: var(--sophos-icon-icon-image-height, 50px);
+    margin: var(--sophos-icon-icon-image-margin, auto);
+    border: var(--sophos-icon-icon-image-border, 0);
+    border-radius: var(--sophos-icon-icon-image-border-radius, 0);
+    cursor: var(--sophos-icon-icon-image-cursor, auto);
   }
 
   #main-container[icon-direction = "left"] {
